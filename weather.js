f07d5b63489cb2a3100e04d92ee7226601653673
Stanislav Dashkevitch <stasdaskevic6@gmail.com>
2024-04-25 22:26:10 +0300

    add storage service

--- weather.js
+++ weather.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js"
 import { printError, printHelp, printSuccess } from "./services/log.service.js"
+import { saveKeyValue } from "./services/storage.service.js"
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Token saved')
+  } catch (e) {
+    printError(e.message) 
+  }
+}
 
 const initCli = () => {
   const args = getArgs(process.argv)
+  if (args.h) {
+    printHelp()
+  } else if (args.s) {
+    printSuccess('dd')
+  } else if (args.t) {
+    return saveToken(args.t)
+  }
   console.log(args)
-  printError('Ho')
-  printSuccess('dd')
-  printHelp()
 }
 
 initCli()
